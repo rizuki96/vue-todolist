41c93c907944c5397d1561a44ee438be5ce19c99
Rizky <rizkyfirmansyah967@gmail.com>
2020-06-21 06:52:00 +0700

    implement v-for and refactor id into props

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,12 @@
   <div id="app">
     <h1>To-Do List</h1>
     <ul>
-      <li>
-        <to-do-item label="My ToDo Item" :done="true"></to-do-item>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item
+          :label="item.label"
+          :done="item.done"
+          :id="item.id"
+        ></to-do-item>
       </li>
     </ul>
   </div>
@@ -11,12 +15,27 @@
 
 <script>
 import ToDoItem from "./components/ToDoItem";
+import uniqueId from "lodash.uniqueid";
 
 export default {
   name: "App",
   components: {
-    ToDoItem
-  }
+    ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId("todo-"), label: "Learn Vue", done: false },
+        {
+          id: uniqueId("todo-"),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { id: uniqueId("todo-"), label: "Have fun", done: true },
+        { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
+      ],
+    };
+  },
 };
 </script>
 
