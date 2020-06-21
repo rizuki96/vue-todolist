a6583eaea441d345f58738901e9a8513bb0384a5
Rizky <rizkyfirmansyah967@gmail.com>
2020-06-21 07:15:40 +0700

    Add ToDoForm component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item
@@ -15,12 +16,14 @@
 
 <script>
 import ToDoItem from "./components/ToDoItem";
+import ToDoForm from "./components/ToDoForm";
 import uniqueId from "lodash.uniqueid";
 
 export default {
   name: "App",
   components: {
     ToDoItem,
+    ToDoForm,
   },
   data() {
     return {
@@ -35,6 +38,15 @@ export default {
         { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
       ],
     };
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      this.ToDoItems.push({
+        id: uniqueId("todo-"),
+        label: toDoLabel,
+        done: false,
+      });
+    },
   },
 };
 </script>
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <label for="new-todo-input">
+        What needs to be done?
+      </label>
+      <input
+        type="text"
+        id="new-todo-input"
+        name="new-todo"
+        autocomplete="off"
+        v-model.trim.lazy="label"
+      />
+      <button type="submit">Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSubmit() {
+      if (this.label === "") {
+        return;
+      }
+      this.$emit("todo-added", this.label);
+      this.label = "";
+    },
+  },
+  data() {
+    return {
+      label: "",
+    };
+  },
+};
+</script>
